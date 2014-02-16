1b48144a320436d6360a2c3f8cff8cc348e6a382
l0nger <rdx.said@gmail.com>
2014-02-16 17:03:02 +0100

    Added initial module core and few functions to module database

---
+++ resources/[polishrp]/prp_core/s_core.lua
@@ -0,0 +1,11 @@
+--[[
+	@desc: (todo)
+	@file: s_core.lua
+	@author: l0nger <l0nger.programmer@gmail.com>
+	@link package: https://github.com/l0nger/PolishRP
+	@licence: GPLv2
+	
+	(c) <2014>, <l0nger.programmer@gmail.com>
+--]]
+
+local root=getRootElement()
---
+++ resources/[polishrp]/prp_db/s_db.lua
@@ -0,0 +1,69 @@
+--[[
+	@desc: (todo)
+	@file: s_db.lua
+	@author: l0nger <l0nger.programmer@gmail.com>
+	@link package: https://github.com/l0nger/PolishRP
+	@licence: GPLv2
+	
+	(c) <2014>, <l0nger.programmer@gmail.com>
+--]]
+
+local ALIVE_TIME_REPEAT = 45000 -- czas (ms) do sprawdzenia polaczenia z baza danych
+local SQL_USER = "username"
+local SQL_PASSWD = "password"
+local SQL_DB = "database"
+local SQL_HOST = "host"
+local SQL_PORT = tonumer(get("port") or 3306)
+
+local db_handler=nil
+local root=getRootElement()
+
+-- -------------------------------------------------------
+
+local function connectWithDB()
+	db_handle=mysql_connect(SQL_HOST, SQL_USER, SQL_PASSWD, SQL_DB, SQL_PORT
+	if not db_handler then
+		outputServerLog("Brak polaczenia z baza danych!")
+	else 
+		outputServerLog("Polaczono z baza danych!")
+	end
+end
+
+local function keepAliveDB()
+	if not mysql_ping(db_handler) then
+		connectWithDB()
+		-- jakies informacje o zerwaniu polaczenia?
+	end
+end
+
+function getHandler()
+	return db_handler
+end
+
+function insertID()
+	mysql_insert_id(db_handler)
+end
+
+function affectedRows()
+	return mysql_affected_rows(db_handler)
+end
+
+function query(str)
+	local res = mysql_query(db_handler, str)
+	if res then 
+		mysql_free_result(result) 
+	end
+	return
+end
+
+function escapeString(val)
+	return mysql_escape_string(db_handler, val)
+end
+
+-- -------------------------------------------------------
+
+addEventHandler("onResourceStart", getResourceRootElement(), 
+	function()
+		connectWithDB()
+		setTimer(keepAliveDB, ALIVE_TIME_REPEAT, 0)
+end)
